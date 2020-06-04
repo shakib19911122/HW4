1825bbbf4064364fcebc4dd3c7cbcf65aec3d06c
shakib <shakib1122@gmail.com>
2020-06-04 18:16:50 +1000

    js update

---
+++ codeQuiz.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+        question: "What does HTML stand for?",
+        choices: ["Home Tool Markup Language", "Hyper Text Markup Language", "Hyperlinks and Text Markup Language", "I don't know"],
+        answer: "Hyper Text Markup Language"
+    },
+    {
+        question: "testing 2+2=?",
+        choices: ["0", "1", "2", "4"],
+        answer: "4 "
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<java>", "<script", "js", "I don't know"],
+        answer: "Hyper Text Markup Language"
+    }]
+
+//timer start once the "Start Quiz" button is click
+
+
+
+// start the quiz
+//  set the function and loop the quiz
+
+for (i=0; i < questions.length; i++){
+    console.log(questions[i]);
+}
+
