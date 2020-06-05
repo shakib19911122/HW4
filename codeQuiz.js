90d509d21a20a2e0825d61273cf8a28215f8c76a
shakib <shakib1122@gmail.com>
2020-06-06 00:27:57 +1000

    js upload

--- codeQuiz.js
+++ codeQuiz.js
@@ -5,24 +5,173 @@ var questions = [
         answer: "Hyper Text Markup Language"
     },
     {
-        question: "testing 2+2=?",
-        choices: ["0", "1", "2", "4"],
-        answer: "4 "
+        question: "What does js stand for?",
+        choices: ["just stop", "just stand", "javascript", "I dont know"],
+        answer: "javascript"
     },
     {
-        question: "Inside which HTML element do we put the JavaScript?",
-        choices: ["<java>", "<script", "js", "I don't know"],
-        answer: "Hyper Text Markup Language"
+        question: "Where do we upload our weekly homework?",
+        choices: ["git up", "git down", "git help", "git hub"],
+        answer: "git hub"
+    },
+    {
+        question: "How to debug?",
+        choices: ["console.log", "you can't debug", "google it", "I dont know"],
+        answer: "console.log"
+    },
+    {
+        question: "The getElementById() method returns the element that has the ___ attribute with the specified value",
+        choices: ["id", "class", "div", "I don't know"],
+        answer: "id"
     }]
 
-//timer start once the "Start Quiz" button is click
 
 
 
-// start the quiz
-//  set the function and loop the quiz
 
-for (i=0; i < questions.length; i++){
-    console.log(questions[i]);
+var score = 0;
+var currentQuestion = -1;
+var timeLeft = 0;
+var timer;
+
+
+document.getElementById ("start")
+
+
+function start() {
+
+    timeLeft = 60;
+    document.getElementById("timeLeft").innerHTML = timeLeft;
+
+    timer = setInterval(function() {
+        timeLeft--;
+        document.getElementById("timeLeft").innerHTML = timeLeft;
+        //proceed to end the game function when timer is below 0 at any time
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            endGame(); 
+        }
+    }, 1000);
+
+    next();
 }
 
+function endGame() {
+    clearInterval(timer);
+
+    var quizContainer = `
+    <h2>Game over!</h2>
+    <h3>You got a ` + score +  ` /100!</h3>
+    <h3>That means you got ` + score / 20 +  ` questions correct!</h3>
+    <input type="text" id="name" placeholder="First name"> 
+    <button onclick="setScore()">Set score!</button>`;
+
+    document.getElementById("quiz-container").innerHTML = quizContainer;
+}
+    
+
+
+//76~86
+function getScore() {
+    var quizContent = `
+    <h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
+    <h1>` + localStorage.getItem("highscore") + `</h1><br> 
+    
+    <button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
+    
+    `;
+
+    document.getElementById("quiz-container").innerHTML = quizContent;
+}
+
+function setScore() {
+    localStorage.setItem("highscore", score);
+    localStorage.setItem("highscoreName",  document.getElementById('name').value);
+    getScore();
+}
+
+
+
+function getScore() {
+    var quizContent = `
+    <h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
+    <h1>` + localStorage.getItem("highscore") + `</h1><br> 
+    
+    <button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
+    
+    `;
+
+    document.getElementById("quiz-container").innerHTML = quizContent;
+}
+
+
+//89~95
+
+function clearScore() {
+    localStorage.setItem("highscore", "");
+    localStorage.setItem("highscoreName",  "");
+
+    resetGame();
+}
+
+
+//97~116
+function resetGame() {
+    clearInterval(timer);
+    score = 0;
+    currentQuestion = -1;
+    timeLeft = 0;
+    timer = null;
+
+    document.getElementById("timeLeft").innerHTML = timeLeft;
+
+    var quizContent = `
+    <h1>
+        JavaScript Quiz!
+    </h1>
+    <h3>
+        Click to play!   
+    </h3>
+    <button onclick="start()">Start!</button>`;
+
+    document.getElementById("quiz-container").innerHTML = quizContent;
+}
+
+//119~154
+
+function incorrect() {
+    timeLeft -= 15; 
+    next();
+}
+
+//increases the score by 20points if the user chooses the correct answer
+function correct() {
+    score += 20;
+    next();
+}
+
+//loops through the questions 
+function next() {
+    currentQuestion++;
+
+    if (currentQuestion > questions.length - 1) {
+        endGame();
+        return;
+    }
+
+    var quizContent = "<h2>" + questions[currentQuestion].question + "</h2>"
+
+    for (var buttonLoop = 0; buttonLoop < questions[currentQuestion].choices.length; buttonLoop++) {
+        var buttonCode = "<button onclick=\"[ANS]\">[CHOICE]</button>"; 
+        buttonCode = buttonCode.replace("[CHOICE]", questions[currentQuestion].choices[buttonLoop]);
+        if (questions[currentQuestion].choices[buttonLoop] == questions[currentQuestion].answer) {
+            buttonCode = buttonCode.replace("[ANS]", "correct()");
+        } else {
+            buttonCode = buttonCode.replace("[ANS]", "incorrect()");
+        }
+        quizContent += buttonCode
+    }
+
+
+    document.getElementById("quiz-container").innerHTML = quizContent;
+}
